c7b2bbcea8fa90f481a5966c423f4438ef319408
Marufhossain07 <marufprogrammer07@gmail.com>
2024-05-11 22:20:16 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+      origin: [
+        "http://localhost:5173",
+      ],
+      credentials: true,
+    })
+  );
 app.use(express.json());
 
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vo0jwvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,11 +29,25 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  };
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    app.get('/jwt', (req,res)=>{
+        const user = req.body;
+        const token = jwt.sign(user, process.env.SECRET_KEY,{
+            expiresIn: '1d'
+        })
+        res.cookie('token', token, cookieOptions)
+        
+        .send({success : true})
+    })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
